628bf518aefb64dfa06400cc511700b4f13a13d3
harshitverma2103 <harshitverma2103@gmail.com>
2024-05-21 12:50:56 +0530

    format code

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import "./Navbar.css";
-import Quaprix from '../../assets/quaprix.png'
+import Quaprix from "../../assets/quaprix.png";
 
 export const Navbar = () => {
   return (
     <header className="container">
       <img className="logo" src={Quaprix} alt="" />
       <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">About Us</a></li>
-        <li><a href="#">Contact Us</a></li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">About Us</a>
+        </li>
+        <li>
+          <a href="#">Contact Us</a>
+        </li>
       </ul>
     </header>
   );
