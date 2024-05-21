8fa95b06023c1f0602923556f8483688af3564c6
harshitverma2103 <harshitverma2103@gmail.com>
2024-05-21 13:22:20 +0530

    creta tagline component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Navbar } from "./Component/Navbar/Navbar";
 import { Hero } from "./Component/Hero/Hero";
+import { Tagline } from "./Component/Tagline/Tagline";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Tagline />
     </div>
   );
 };
---
+++ src/Component/Tagline/Tagline.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Tagline.css";
+import quote_left from "../../assets/quote-left.png";
+import points from "../../assets/point.png";
+
+export const Tagline = () => {
+  return (
+    <div className="tagline">
+      <img className="quote" src={quote_left} alt="" />
+      <h1>
+        Your vision <br />
+        Our responsibility
+      </h1>
+      <img className="points" src={points} alt="" />
+    </div>
+  );
+};
