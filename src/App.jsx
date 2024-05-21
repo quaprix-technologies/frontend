2a4a5a5f182b11fe053e4418baad296832b095f0
harshitverma2103 <harshitverma2103@gmail.com>
2024-05-21 12:32:54 +0530

    create hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Navbar } from "./Component/Navbar/Navbar";
+import { Hero } from "./Component/Hero/Hero";
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <Hero />
     </div>
   );
 };
---
+++ src/Component/Hero/Hero.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Hero.css";
+import video from "../../assets/hero_section.mp4";
+
+export const Hero = () => {
+  return (
+    <div className="hero">
+      <video src={video} autoPlay loop muted>
+        <source src={video} type="video/mp4" />
+      </video>
+      <div className="text-wrap">
+        <h1>
+          STREAMLINE YOUR
+          <br />
+          IT OPERATION
+        </h1>
+        <p>
+          Efficient and cost-effective IT services for <br /> businesses of all sizes.
+        </p>
+      </div>
+      <button className="bottom-btn">Learn More</button>
+    </div>
+  );
+};
