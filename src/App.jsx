4e5df2487ce89ca785663fd77568b9f952071683
harshitverma2103 <harshitverma2103@gmail.com>
2024-05-21 11:46:57 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Navbar } from "./Component/Navbar/Navbar";
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Navbar.css";
+import Quaprix from '../../assets/quaprix.png'
+
+export const Navbar = () => {
+  return (
+    <header className="container">
+      <img className="logo" src={Quaprix} alt="" />
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+    </header>
+  );
+};
