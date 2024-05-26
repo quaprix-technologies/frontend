d11aaf0b4a14885fa86f93deb280cadb20f361e0
harshitverma2103 <harshitverma2103@gmail.com>
2024-05-26 17:39:14 +0530

    extract Header and Footer from Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 import React from "react";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Homepage />,
-  },
-]);
+import { Header } from "./Component/Header/Header";
+import { Footer } from "./Component/Footer/Footer";
 
 const App = () => {
-  return (
-    <RouterProvider router={router} />
-  );
+  const FullPage = ({ children }) => {
+    return (
+      <>
+        <Header />
+        {children}
+        <Footer />
+      </>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <FullPage>
+          <Homepage />
+        </FullPage>
+      ),
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 };
 
 export default App;
