f82c461e88fa8905a017f6a17e06c25b86af456e
harshitverma2103 <harshitverma2103@gmail.com>
2024-05-28 13:47:52 +0530

    About us content

--- src/pages/AboutUs/index.jsx
+++ src/pages/AboutUs/index.jsx
@@ -1,22 +1,45 @@
 import React from "react";
 import "./About.css";
 
-const index = () => {
+const About = () => {
   return (
-    <div className="background-container">
-      <div className="text-container">
-        <h1>
-          Open Your World <br />
-          Of Opportunities
-          <br />
-          With us
-        </h1>
-        <button className="get-started-btn">
-          <a href="">Get Started now</a>
-        </button>
+    <>
+      <div className="background-container">
+        <div className="text-container">
+          <h1>
+            Open Your World <br />
+            Of Opportunities
+            <br />
+            With us
+          </h1>
+          <button className="get-started-btn">
+            <a href="/contact-us">Get Started now</a>
+          </button>
+        </div>
       </div>
-    </div>
+      <div className="aboutus-text about-container">
+        <h1>About Us</h1>
+        <p>
+          At Quaprix, we're dedicated to providing cutting-edge information
+          technology (IT) services that drive your business forward. With our
+          expertise and commitment to excellence, we offer a comprehensive suite
+          of services tailored to meet your unique needs and propel your success
+          in today's digital landscape.
+        </p>
+        <p>
+          Working as a partner with our clients, we are dedicated to developing
+          effective outsourcing partnerships in order to reduce operational cost
+          and empowering them to devote more time to their core business.
+        </p>
+        <p>
+          We have served different industries like field service management,
+          IoT-Utilities, healthcare, predictive analysis, financial services,
+          retail & eCommerce, blockchain, and workflow automation etc. We
+          deliver solutions that open a world of possibilities for our clients.
+        </p>
+      </div>
+    </>
   );
 };
 
-export default index;
+export default About;
