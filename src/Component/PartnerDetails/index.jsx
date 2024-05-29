4cf10899c8f8060cca7d8ba54acf11e936a129e8
harshitverma2103 <harshitverma2103@gmail.com>
2024-05-29 18:05:50 +0530

    format document

--- src/Component/PartnerDetails/index.jsx
+++ src/Component/PartnerDetails/index.jsx
@@ -6,7 +6,7 @@ const PartnerDetails = () => {
   return (
     <div className="partnersdetails">
       <h2>Our Partner </h2>
-      <a href="https://www.qualitydynamics.net/"  target="_blank" >
+      <a href="https://www.qualitydynamics.net/" target="_blank">
         <img src={partner_iocn} alt="" className="partner-icon" />
       </a>
     </div>
