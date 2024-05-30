0fbfdfb426a0addcbde84b668c93a86c8716c96f
harshitverma2103 <harshitverma2103@gmail.com>
2024-05-30 22:58:20 +0530

    add cards

--- src/pages/Service/index.jsx
+++ src/pages/Service/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import "./service.css";
+import Cards from "./Cards/Cards";
 
 const index = () => {
 
@@ -8,16 +9,19 @@ const index = () => {
   }, []);
 
   return (
-    <div className="service">
-      <div className="text">
-        <h1>What We Do</h1>
-        <p>
-          We offer a comprehensive range of IT services, <br />
-          Our services are designed to help small <br />
-          businesses thrive in today's digital world.
-        </p>
+    <>
+      <div className="service">
+        <div className="text">
+          <h1>What We Do</h1>
+          <p>
+            We offer a comprehensive range of IT services, <br />
+            Our services are designed to help small <br />
+            businesses thrive in today's digital world.
+          </p>
+        </div>
       </div>
-    </div>
+      <Cards />
+    </>
   );
 };
 
