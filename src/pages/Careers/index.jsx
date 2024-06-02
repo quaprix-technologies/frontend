cbe98db16f741d8ab606756527a267ef2fb67e9c
harshitverma2103 <harshitverma2103@gmail.com>
2024-06-02 15:07:13 +0530

    add jobs

--- src/pages/Careers/index.jsx
+++ src/pages/Careers/index.jsx
@@ -5,6 +5,38 @@ const index = () => {
   useEffect(() => {
     document.title = "careers";
   });
+
+  const jobs = [
+    {
+      title: "Web Developer",
+      description: "Experience 3+ years",
+      postType: "Full-time-Contract",
+      location: "Remote",
+      pdfUrl: "/WEB_DEVELOPER.pdf",
+    },
+    {
+      title: "QA Engineer",
+      description: "Experience 3+ years",
+      postType: "Full-time-Contract",
+      location: "Remote",
+      pdfUrl: "/QA.pdf",
+    },
+  ];
+
+  const downloadJobDescription = (pdfUrl) => {
+    try {
+      const fullUrl = `${window.location.origin}${pdfUrl}`;
+      const a = document.createElement("a");
+      a.href = fullUrl;
+      a.download = pdfUrl.substring(pdfUrl.lastIndexOf("/") + 1);
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    } catch (error) {
+      console.error("Error downloading PDF:", error);
+    }
+  };
+
   return (
     <div className="career">
       <div className="career-background">
@@ -15,6 +47,29 @@ const index = () => {
             happen!
           </p>
         </div>
+      </div>
+      <div className="open-position">
+        <h1>Open Positions</h1>
+        <h3>
+          Take a look at our open positions below to find something that excites
+          you.
+        </h3>
+      </div>
+      <div className="jobs">
+        {jobs.map((job, index) => (
+          <div key={index} className="job-listing">
+            <h2>{job.title}</h2>
+            <p>{job.description}</p>
+            <p>Location: {job.location}</p>
+            <p>Post Type: {job.postType}</p>
+            <button
+              className="apply-button"
+              onClick={() => downloadJobDescription(job.pdfUrl)}
+            >
+              Apply Now
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
