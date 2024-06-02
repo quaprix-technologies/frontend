5aeab29afdfedf0a71a0ac6e5ef765e8a44fdd64
harshitverma2103 <harshitverma2103@gmail.com>
2024-06-03 01:53:01 +0530

    create sperate component for contact-form

--- src/pages/ContactUs/index.jsx
+++ src/pages/ContactUs/index.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from "react";
 import "./ContactUs.css";
+import Contact from "../../Component/Contact_Footer/Contact"
 import msgIcon from "../../assets/msg-icon.png";
-import mailIcon from "../../assets/mail-icon.png";
-import phoneIcon from "../../assets/phone-icon.png";
-import locationIcon from "../../assets/location-icon.png";
 import backgroundImg from "../../assets/ContactUs.png";
+import Form from "../../Component/ContactForm/Form";
 
 const ContactUs = () => {
 
@@ -32,63 +31,9 @@ const ContactUs = () => {
             important to us as we strive to provide exceptional service to our
             university community.
           </p>
-          <ul>
-            <li key="email">
-              <img src={mailIcon} alt="Mail Icon" />
-              info@quaprix.com
-            </li>
-            <li key="phone">
-              <img src={phoneIcon} alt="Phone Icon" />
-              +91-9958413035
-            </li>
-            <li key="address">
-              <img src={locationIcon} alt="Location Icon" />
-              D-A-5, Veer Savarkar Block,
-              <br /> Dayanand Colony, Shakarpur,
-              <br /> New Delhi, Delhi, 110092
-            </li>
-          </ul>
+          <Contact/>
         </div>
-        <div className="contact-col">
-          <form>
-            <label htmlFor="name">Your Name</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Enter your name"
-              required
-            />
-            <label htmlFor="phone">Phone</label>
-            <input
-              type="tel"
-              id="phone"
-              name="phone"
-              placeholder="Enter your phone number"
-              required
-            />
-            <label htmlFor="email">Your Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Enter your email"
-              required
-            />
-            <label htmlFor="message">Write Your Message Here</label>
-            <textarea
-              id="message"
-              name="message"
-              cols="30"
-              rows="6"
-              placeholder="Enter your message"
-              required
-            ></textarea>
-            <button type="submit" className="btn dark-btn">
-              Submit Now
-            </button>
-          </form>
-        </div>
+        <Form/>
       </div>
     </div>
   );
