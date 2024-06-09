545f6a62f58ac12d5ea9be618d27cb5592648914
harshitverma2103 <harshitverma2103@gmail.com>
2024-06-09 14:16:58 +0530

    feature contact-page-scroll

--- src/components/ContactButton/index.jsx
+++ src/components/ContactButton/index.jsx
@@ -1,13 +1,29 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import "./styles.css";
 
 const ContactButton = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleOnClick = () => {
     navigate("/contact-us");
+    if (location.pathname === "/contact-us") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
   };
+
+  useEffect(() => {
+    if (location.pathname === "/contact-us") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  }, [location.pathname]);
 
   return (
     <div>
