5110076293dd09f7d033c51c539464ff79b2834c
harshitverma2103 <harshitverma2103@gmail.com>
2024-06-09 12:58:59 +0530

    Used the navigator to go to the Contact Us page

--- src/components/ContactButton/index.jsx
+++ src/components/ContactButton/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import "./styles.css"
+import { useNavigate } from "react-router-dom";
+import "./styles.css";
 
 const ContactButton = () => {
+  const navigate = useNavigate();
+
+  const handleOnClick = () => {
+    navigate("/contact-us");
+  };
+
   return (
     <div>
-      <button className="contact-btn">
-        <a href="/contact-us">Contact</a>
+      <button className="contact-btn" onClick={handleOnClick}>
+        Contact
       </button>
     </div>
   );
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import video from "../../assets/hero-images/hero-section.mp4";
 import "./styles.css";
 
 const Hero = () => {
+  const navigate = useNavigate();
+
+  const handleOnClick = () => {
+    navigate("/contact-us");
+  };
+
   return (
     <div className="hero">
       <video src={video} autoPlay loop muted>
@@ -15,10 +22,13 @@ const Hero = () => {
           IT OPERATION
         </h1>
         <p>
-          Efficient and cost-effective IT services for <br /> businesses of all sizes.
+          Efficient and cost-effective IT services for <br /> businesses of all
+          sizes.
         </p>
       </div>
-      <button className="bottom-btn">Get Started</button>
+      <button className="bottom-btn" onClick={handleOnClick}>
+        Get Started
+      </button>
     </div>
   );
 };
