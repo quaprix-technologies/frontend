45debb573202320ac7f3c74983a7e10a9f01e13c
harshitverma2103 <harshitverma2103@gmail.com>
2024-06-10 13:39:53 +0530

    Add whyUs-data file for whyUs Card data and fix whyUs card styles

--- src/components/WhyUsCards/index.jsx
+++ src/components/WhyUsCards/index.jsx
@@ -1,30 +1,21 @@
 import React from "react";
 import Card from "../Card";
+import { data } from "./whyUs-data.js";
 import ContactButton from "../ContactButton";
-import teams from "../../assets/icons/group.png";
-import mortarboard from "../../assets/icons/mortarboard.png";
-import tech from "../../assets/icons/coding.png";
+
 import "./styles.css";
 
 const WhyUsCards = () => {
   return (
     <div className="why-us-card-container">
-      <Card
-        icon={teams}
-        title="Our Story"
-        content="We started as a small team of IT enthusiasts who wanted to help small businesses overcome their technology challenges. Today, we have grown into a leading provider of IT services."
-      />
-      <Card
-        icon={mortarboard}
-        title="Our Mission"
-        content="To revolutionize the IT industry by pushing the boundaries of innovation. We strive to be at the forefront of technological advancement, continuously pioneering new solutions that empower businesses and individuals to thrive in a rapidly evolving digital landscape."
-      />
-      <Card
-        icon={tech}
-        title="Technologies"
-        content="We offer a comprehensive range of IT services. Our services are designed to help small businesses thrive in today's digital world."
-        url="Learn More"
-      />
+      {data.map((cardData, index) => {
+        const { icon, title, content, url } = cardData;
+        return (
+          <div key={index} className="why-us-card">
+            <Card icon={icon} title={title} content={content} url={url} />
+          </div>
+        );
+      })}
       <div className="why-us-card-button">
         <ContactButton />
       </div>
---
+++ src/components/WhyUsCards/whyUs-data.js
@@ -0,0 +1,25 @@
+import group from "../../assets/icons/group.png";
+import mortarboard from "../../assets/icons/mortarboard.png";
+import tech from "../../assets/icons/coding.png";
+
+export const data = [
+  {
+    icon: group,
+    title: "Our Story",
+    content:
+      "We started as a small team of IT enthusiasts who wanted to help small businesses overcome their technology challenges. Today, we have grown into a leading provider of IT services.",
+  },
+  {
+    icon: mortarboard,
+    title: "Our Mission",
+    content:
+      "To revolutionize the IT industry by pushing the boundaries of innovation. We strive to be at the forefront of technological advancement, continuously pioneering new solutions that empower businesses and individuals to thrive in a rapidly evolving digital landscape.",
+  },
+  {
+    icon: tech,
+    title: "Technologies",
+    content:
+      "We offer a comprehensive range of IT services. Our services are designed to help small businesses thrive in today's digital world.",
+    url: "Learn More",
+  },
+];
