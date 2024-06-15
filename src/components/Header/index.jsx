6f0f3447235e6f2ab0856b3cd08df945db4909f2
prince-verma <prince.verma@rea-group.com>
2024-06-15 18:52:23 +0530

    Created a sepearte component to handle outside click

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, {useState} from "react";
 import {useNavigate} from "react-router-dom";
+import OutsideClickHandler from "../OutsideClickHandler";
 import NavbarItem from "./NavbarItem";
 import {HOME_PAGE_PATH, SERVICES_PAGE_PATH, ABOUT_US_PAGE_PATH, CONTACT_US_PAGE_PATH, CAREERS_PAGE_PATH} from "../../constants.js";
 import quaprix from "../../assets/icons/quaprix.png";
@@ -7,11 +8,11 @@ import menu_icon from "../../assets/icons/menu-icon.png";
 import "./styles.css";
 
 const Header = () => {
-  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
   const navigate = useNavigate();
 
   const toggleMenu = () => {
-    setMobileMenu((prevMobileMenu) => !prevMobileMenu);
+    setShowMobileMenu((prevMobileMenu) => !prevMobileMenu);
   };
 
   const handleOnClick = (path) => {
@@ -19,32 +20,20 @@ const Header = () => {
     navigate(path);
   };
 
-  useEffect(() => {
-    const outsideClickHandler = (event) => {
-      if (mobileMenu && !event.target.closest(".header-container")) {
-        setMobileMenu(false);
-      }
-    };
-
-    if (mobileMenu) {
-      document.addEventListener("click", outsideClickHandler);
-    } else {
-      document.removeEventListener("click", outsideClickHandler);
-    }
-  }, [mobileMenu]);
-
   return (
-      <header className="header-container">
-          <img className="quaprix-logo" src={quaprix} alt="image not found" onClick={() => handleOnClick(HOME_PAGE_PATH)}/>
-          <ul className={mobileMenu ? "show-mobile-menu" : "hide-mobile-menu"}>
-              <NavbarItem path={HOME_PAGE_PATH} onClick={handleOnClick}>Home</NavbarItem>
-              <NavbarItem path={SERVICES_PAGE_PATH} onClick={handleOnClick}>Services</NavbarItem>
-              <NavbarItem path={ABOUT_US_PAGE_PATH} onClick={handleOnClick}>About Us</NavbarItem>
-              <NavbarItem path={CONTACT_US_PAGE_PATH} onClick={handleOnClick}>Contact Us</NavbarItem>
-              <NavbarItem path={CAREERS_PAGE_PATH} onClick={handleOnClick}>Careers</NavbarItem>
-          </ul>
-          <img src={menu_icon} alt="Menu icon" className="menu-icon" onClick={toggleMenu}/>
-      </header>
+      <OutsideClickHandler onOutsideClick={() => setShowMobileMenu(false)}>
+          <header className="header-container">
+              <img className="quaprix-logo" src={quaprix} alt="image not found" onClick={() => handleOnClick(HOME_PAGE_PATH)}/>
+              <ul className={showMobileMenu ? "show-mobile-menu" : "hide-mobile-menu"}>
+                  <NavbarItem path={HOME_PAGE_PATH} onClick={handleOnClick}>Home</NavbarItem>
+                  <NavbarItem path={SERVICES_PAGE_PATH} onClick={handleOnClick}>Services</NavbarItem>
+                  <NavbarItem path={ABOUT_US_PAGE_PATH} onClick={handleOnClick}>About Us</NavbarItem>
+                  <NavbarItem path={CONTACT_US_PAGE_PATH} onClick={handleOnClick}>Contact Us</NavbarItem>
+                  <NavbarItem path={CAREERS_PAGE_PATH} onClick={handleOnClick}>Careers</NavbarItem>
+              </ul>
+              <img src={menu_icon} alt="Menu icon" className="menu-icon" onClick={toggleMenu}/>
+          </header>
+      </OutsideClickHandler>
   );
 };
 
