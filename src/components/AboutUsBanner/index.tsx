f0d45469698a1b16856918b4fa96b5c0e2dc2acd
harshitverma2103 <harshitverma2103@gmail.com>
2024-06-28 08:42:27 +0530

    Convert AboutUsBanner components to tsx

--- src/components/AboutUsBanner/index.tsx
+++ src/components/AboutUsBanner/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ContactButton from "../ContactButton";
 import "./styles.css";
 
-const AboutUsBanner = () => {
+const AboutUsBanner: React.FC = () => {
   return (
     <div className="about-us-banner">
       <h1>Get In Touch With Us</h1>
